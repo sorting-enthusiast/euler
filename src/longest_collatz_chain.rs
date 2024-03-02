e6ad7b26832edf2e420e218851c88a1265c600a0
Ronnie Rasamat <delta7.athersprite@gmail.com>
2024-03-02 18:17:11 +0200

    project euler problem 14

---
+++ src/longest_collatz_chain.rs
@@ -0,0 +1,76 @@
+use std::collections::HashMap;
+#[derive(Default)]
+pub struct DynamicProgrammingCollatz {
+    lookup_table: HashMap<usize, usize>,
+}
+impl DynamicProgrammingCollatz {
+    pub fn chain_length(&mut self, mut x: usize) -> usize {
+        let original_x = x;
+        let mut seq_length = 1;
+        if x & 1 == 0 {
+            let shift_amt = x.trailing_zeros() as usize;
+            x >>= shift_amt;
+            seq_length += shift_amt;
+        }
+        while x > 1 {
+            if let Some(&len) = self.lookup_table.get(&x) {
+                return seq_length + len - 1;
+            }
+            x += (x + 1) >> 1;
+            let shift_amt = x.trailing_zeros() as usize;
+            x >>= shift_amt;
+            seq_length += shift_amt + 2;
+        }
+        self.lookup_table.insert(original_x, seq_length);
+        seq_length
+    }
+}
+//use for compile time evaluation
+pub const fn collatz_chain_length(mut x: usize) -> usize {
+    let mut seq_length = 1;
+    if x & 1 == 0 {
+        let shift_amt = x.trailing_zeros() as usize;
+        x >>= shift_amt; //equivalent to repeatedly dividing by 2 until reaching an odd number, adds shift_amt to seq_length
+
+        seq_length += shift_amt;
+    }
+    while x > 1 {
+        x += (x + 1) >> 1; //equivalent to (3x+1)/2, adds 2 to seq_length
+
+        let shift_amt = x.trailing_zeros() as usize;
+        x >>= shift_amt; //equivalent to repeatedly dividing by 2 until reaching an odd number, adds shift_amt to seq_length
+
+        seq_length += shift_amt + 2;
+    }
+    seq_length
+}
+pub trait Collatz {
+    fn collatz_chain_length(self) -> usize;
+}
+macro_rules! gen_collatz_impl {
+    ($($type:ty),+) => { $(
+        impl Collatz for $type {
+            fn collatz_chain_length(self) -> usize {
+                let mut x = self;
+                let mut seq_length = 1;
+                if x & 1 == 0 {
+                    let shift_amt = x.trailing_zeros() as usize;
+                    x >>= shift_amt; //equivalent to repeatedly dividing by 2 until reaching an odd number, adds shift_amt to seq_length
+
+                    seq_length += shift_amt;
+                }
+                while x > 1 {
+                    x += (x + 1) >> 1; //equivalent to (3x+1)/2, adds 2 to seq_length
+
+                    let shift_amt = x.trailing_zeros() as usize;
+                    x >>= shift_amt; //equivalent to repeatedly dividing by 2 until reaching an odd number, adds shift_amt to seq_length
+
+                    seq_length += shift_amt + 2;
+                }
+                seq_length
+            }
+        }
+
+    )+ };
+}
+gen_collatz_impl!(u8, u16, u32, u64, u128, usize);
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+pub mod longest_collatz_chain;
+use crate::longest_collatz_chain::{collatz_chain_length, Collatz};
+const N: u32 = 1_000_000;
+
+pub fn main() {
+    let mut max_start = 1;
+    let mut max_chain = 1;
+    //chain(2k) = 1 + chain(k) > chain(k) for all k, hence we can ignore all values < N/2
+    for n in (N / 2)..N {
+        let chain = n.collatz_chain_length();
+        if chain > max_chain {
+            max_chain = chain;
+            max_start = n;
+        }
+    }
+    println!("{:?}", (max_start, max_chain));
+    println!("{:?}", (837799, collatz_chain_length(837799)));
 }
