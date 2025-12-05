91c66c16a2ab4bef6d57b30e0b111f976da352e1
Ronnie Rasamat <delta7.athersprite@gmail.com>
2025-12-06 01:36:07 +0200

    optimized e153

--- src/p100_199/e153.rs
+++ src/p100_199/e153.rs
@@ -1,24 +1,23 @@
-use crate::utils::{
-    FIArray::FIArray, math::gcd, multiplicative_function_summation::dirichlet_mul_usize,
-};
+use crate::utils::{FIArray::FIArray, multiplicative_function_summation::dirichlet_mul_usize};
+
 const N: usize = 1e8 as _;
-const SQRT_N: usize = N.isqrt();
+
+// https://en.wikipedia.org/wiki/Coprime_integers#Generating_all_coprime_pairs
+fn calkin_wilf(m: usize, n: usize, sum_divisors: &FIArray) -> usize {
+    let mmnn = m * m + n * n;
+    if mmnn > N {
+        return 0;
+    }
+    (m + n) * sum_divisors[N / mmnn]
+        + calkin_wilf(m + n, m, sum_divisors)
+        + calkin_wilf(m + n, n, sum_divisors)
+}
+
 pub fn main() {
     let start = std::time::Instant::now();
     let sum_divisors =
         dirichlet_mul_usize(&FIArray::unit(N), &FIArray::id::<{ usize::MAX >> 1 }>(N), N);
-    let mut res = sum_divisors[N / 2];
-    for a in 1..=SQRT_N {
-        for b in a + 1..=SQRT_N {
-            if gcd(a as _, b as _) == 1 {
-                let n = a * a + b * b;
-                if n > N {
-                    break;
-                }
-                res += (a + b) * sum_divisors[N / n];
-            }
-        }
-    }
+    let mut res = sum_divisors[N / 2] + calkin_wilf(2, 1, &sum_divisors);
     res <<= 1;
     res += sum_divisors[N];
     let end = start.elapsed();
