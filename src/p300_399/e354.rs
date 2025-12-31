71f11e8896e3fcd3d4c71724a0fc8a90d4b04e5f
Ronnie Rasamat <delta7.athersprite@gmail.com>
2025-12-31 22:41:27 +0200

    354

--- src/p300_399/e354.rs
+++ src/p300_399/e354.rs
@@ -2,36 +2,46 @@ use crate::utils::primes::wheel_sieve;
 
 const N: u64 = 5e11 as _;
 const SQRT_N: u64 = N.isqrt();
-const MAX_KEY: u64 = N / (3 * 7u64.pow(2) * 13u64.pow(2) * 19);
-const MAX_PRIME: u64 = N / (3 * 7u64.pow(2) * 13u64.pow(2));
+const BOUND_INT: u64 = N / 3;
+const BOUND_SQRT: u64 = ((N as u128).pow(2) / 3).isqrt() as u64;
+const MAX_BOUND: u64 = BOUND_SQRT;
+const MAX_KEY: u64 = MAX_BOUND / (7u64.pow(2) * 13u64.pow(2) * 19);
+const MAX_PRIME: u64 = MAX_BOUND / (7u64.pow(2) * 13u64.pow(2));
+
+fn dfs(lim: u64, primes: &[u64]) -> u64 {
+    let mut sum = 1;
+    for (i, &p) in primes.iter().enumerate() {
+        if p > lim {
+            break;
+        }
+        let mut new_lim = lim;
+        loop {
+            new_lim /= p;
+            sum += dfs(new_lim, &primes[i + 1..]);
+            if p > new_lim {
+                break;
+            }
+        }
+    }
+    sum
+}
 
 pub fn main() {
     let start = std::time::Instant::now();
     let primes = wheel_sieve(MAX_PRIME);
     let (splitting, inert_ramified): (Vec<u64>, Vec<u64>) =
         primes.iter().partition(|&&p| p % 3 == 1);
-    dbg!(&splitting[..10]);
-
     let mut res = 0;
-    let mut smooth = vec![1u64; MAX_KEY as usize];
-    for &p in &inert_ramified {
-        if p * p > MAX_KEY {
-            break;
-        }
-        for v in p..=MAX_KEY {
-            smooth[v as usize - 1] += smooth[(v / p) as usize - 1];
-        }
-    }
-
+    let mut cache = vec![None; MAX_KEY as usize];
     // 2 2 1
     for (i, &p1) in splitting.iter().enumerate() {
-        let lim = N / (p1 * p1);
-        if lim < 3 {
+        let lim = BOUND_INT / (p1 * p1);
+        if lim == 0 {
             break;
         }
         for &p2 in &splitting[i + 1..] {
             let lim = lim / (p2 * p2);
-            if lim < 3 {
+            if lim == 0 {
                 break;
             }
             for &p3 in &splitting {
@@ -39,18 +49,18 @@ pub fn main() {
                     continue;
                 }
                 let lim = lim / p3;
-                if lim < 3 {
+                if lim == 0 {
                     break;
                 }
-                res += smooth[(lim as usize) / 3 - 1];
+                res += *cache[lim as usize - 1].get_or_insert_with(|| dfs(lim, &inert_ramified)); //smooth[lim as usize - 1];
             }
         }
     }
 
     // 7 2
     for &p1 in &splitting {
-        let lim = N / p1.pow(7);
-        if lim < 3 {
+        let lim = BOUND_INT / p1.pow(7);
+        if lim == 0 {
             break;
         }
         for &p2 in &splitting {
@@ -58,16 +68,17 @@ pub fn main() {
                 continue;
             }
             let lim = lim / (p2 * p2);
-            if lim < 3 {
+            if lim == 0 {
                 break;
             }
-            res += smooth[(lim as usize) / 3 - 1];
+            res += *cache[lim as usize - 1].get_or_insert_with(|| dfs(lim, &inert_ramified)); //smooth[lim as usize - 1];
         }
     }
+
     // 12 1
     for &p1 in &splitting {
-        let lim = N / p1.pow(12);
-        if lim < 3 {
+        let lim = BOUND_INT / p1.pow(12);
+        if lim == 0 {
             break;
         }
         for &p2 in &splitting {
@@ -75,11 +86,73 @@ pub fn main() {
                 continue;
             }
             let lim = lim / p2;
-            if lim < 3 {
+            if lim == 0 {
                 break;
             }
-            res += smooth[(lim as usize) / 3 - 1];
+            res += *cache[lim as usize - 1].get_or_insert_with(|| dfs(lim, &inert_ramified)); //smooth[lim as usize - 1];
         }
     }
+
+    // 2 2 1
+    for (i, &p1) in splitting.iter().enumerate() {
+        let lim = BOUND_SQRT / (p1 * p1);
+        if lim == 0 {
+            break;
+        }
+        for &p2 in &splitting[i + 1..] {
+            let lim = lim / (p2 * p2);
+            if lim == 0 {
+                break;
+            }
+            for &p3 in &splitting {
+                if p1 == p3 || p2 == p3 {
+                    continue;
+                }
+                let lim = lim / p3;
+                if lim == 0 {
+                    break;
+                }
+                res += *cache[lim as usize - 1].get_or_insert_with(|| dfs(lim, &inert_ramified)); //smooth[lim as usize - 1];
+            }
+        }
+    }
+
+    // 7 2
+    for &p1 in &splitting {
+        let lim = BOUND_SQRT / p1.pow(7);
+        if lim == 0 {
+            break;
+        }
+        for &p2 in &splitting {
+            if p1 == p2 {
+                continue;
+            }
+            let lim = lim / (p2 * p2);
+            if lim == 0 {
+                break;
+            }
+            res += *cache[lim as usize - 1].get_or_insert_with(|| dfs(lim, &inert_ramified)); //smooth[lim as usize - 1];
+        }
+    }
+
+    // 12 1
+    for &p1 in &splitting {
+        let lim = BOUND_SQRT / p1.pow(12);
+        if lim == 0 {
+            break;
+        }
+        for &p2 in &splitting {
+            if p1 == p2 {
+                continue;
+            }
+            let lim = lim / p2;
+            if lim == 0 {
+                break;
+            }
+            res += *cache[lim as usize - 1].get_or_insert_with(|| dfs(lim, &inert_ramified)); //smooth[lim as usize - 1];
+        }
+    }
+
     println!("res = {res}, took {:?}", start.elapsed());
+    dbg!(cache.iter().flatten().count());
 }
