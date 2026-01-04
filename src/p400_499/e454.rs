47c54bc60bb7faed5718a710b9f5bc0a567e1559
Ronnie Rasamat <delta7.athersprite@gmail.com>
2026-01-04 07:10:05 +0200

    misc

--- src/p400_499/e454.rs
+++ src/p400_499/e454.rs
@@ -3,81 +3,9 @@ use crate::utils::{fast_divisor_sums::icbrt, multiplicative_function_summation::
 const N: i64 = 1e12 as _;
 const SQRT_N: i64 = N.isqrt();
 
-// runs in O(n^2/3)
-fn inner(n: i64) -> i64 {
-    // using inner2 instead of inner1 gives a 4x speedup
-    fn inner1(n: i64) -> i64 {
-        #[inline(always)]
-        fn s(lim: i64, x1: i64, x2: i64) -> i64 {
-            let mut ret = 0;
-            let mut q = x1;
-            while q <= lim.min(x2) {
-                let k = lim / q;
-                let q_next = 1 + (lim / k).min(x2);
-
-                ret += (q_next - q) * k;
-
-                q = q_next;
-            }
-            ret
-        }
-        let mut ret = 0;
-        let nsqrt = n.isqrt();
-        for i in 1..=nsqrt {
-            let lim = n / i;
-            ret += s(lim, i + 1, 2 * i - 1);
-        }
-        ret
-    }
-
-    // adapted from fakesson's forum post
-    fn inner2(n: i64) -> i64 {
-        #[inline(always)]
-        fn s(lim: i64, x1: i64, x2: i64) -> i64 {
-            let mut ret = 0;
-            let mut prev = x1 - 1;
-            let z = lim / x1;
-            let zL = lim / x2;
-
-            for z in (zL + 1..=z).rev() {
-                let curr = lim / z;
-                ret += z * (curr - prev);
-                prev = curr;
-            }
-
-            ret += zL * (x2 - prev);
-            ret
-        }
-
-        let mut ret = 0;
-        let nsqrt = n.isqrt();
-        let ncbrt = icbrt(n);
-        for i in 1..=ncbrt {
-            let lim = n / i;
-            for j in i + 1..2 * i {
-                ret += lim / j;
-            }
-        }
-        for i in ncbrt + 1..=nsqrt {
-            let lim = n / i;
-            ret += s(lim, i + 1, 2 * i - 1);
-        }
-        ret
-    }
-    inner2(n)
-}
-
 pub fn main() {
     solve();
-    let start = std::time::Instant::now();
-    let mob = mobius_sieve(SQRT_N as usize + 1);
-    let mut res = inner(N);
-    for i in 2..=SQRT_N {
-        if mob[i as usize] != 0 {
-            res += i64::from(mob[i as usize]) * inner(N / (i * i));
-        }
-    }
-    println!("res = {res}, took {:?}", start.elapsed());
+    //alt();
 }
 
 // O(n^2/3) time, O(n^1/3) space
@@ -148,5 +76,80 @@ fn solve() {
         res += small_diff[i as usize - 1] * m;
     }
 
+    println!("res = {res}, took {:?}", start.elapsed());
+}
+// runs in O(n^2/3)
+fn inner(n: i64) -> i64 {
+    // using inner2 instead of inner1 gives a 4x speedup
+    fn inner1(n: i64) -> i64 {
+        #[inline(always)]
+        fn s(lim: i64, x1: i64, x2: i64) -> i64 {
+            let mut ret = 0;
+            let mut q = x1;
+            while q <= lim.min(x2) {
+                let k = lim / q;
+                let q_next = 1 + (lim / k).min(x2);
+
+                ret += (q_next - q) * k;
+
+                q = q_next;
+            }
+            ret
+        }
+        let mut ret = 0;
+        let nsqrt = n.isqrt();
+        for i in 1..=nsqrt {
+            let lim = n / i;
+            ret += s(lim, i + 1, 2 * i - 1);
+        }
+        ret
+    }
+
+    // adapted from fakesson's forum post
+    fn inner2(n: i64) -> i64 {
+        #[inline(always)]
+        fn s(lim: i64, x1: i64, x2: i64) -> i64 {
+            let mut ret = 0;
+            let mut prev = x1 - 1;
+            let z = lim / x1;
+            let zL = lim / x2;
+
+            for z in (zL + 1..=z).rev() {
+                let curr = lim / z;
+                ret += z * (curr - prev);
+                prev = curr;
+            }
+
+            ret += zL * (x2 - prev);
+            ret
+        }
+
+        let mut ret = 0;
+        let nsqrt = n.isqrt();
+        let ncbrt = icbrt(n);
+        for i in 1..=ncbrt {
+            let lim = n / i;
+            for j in i + 1..2 * i {
+                ret += lim / j;
+            }
+        }
+        for i in ncbrt + 1..=nsqrt {
+            let lim = n / i;
+            ret += s(lim, i + 1, 2 * i - 1);
+        }
+        ret
+    }
+    inner2(n)
+}
+
+fn alt() {
+    let start = std::time::Instant::now();
+    let mob = mobius_sieve(SQRT_N as usize + 1);
+    let mut res = inner(N);
+    for i in 2..=SQRT_N {
+        if mob[i as usize] != 0 {
+            res += i64::from(mob[i as usize]) * inner(N / (i * i));
+        }
+    }
     println!("res = {res}, took {:?}", start.elapsed());
 }
